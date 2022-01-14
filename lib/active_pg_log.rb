209ac54047c169886552825a9bd132c1d28e9390
Wesley Oliveira <waejloliveira@gmail.com>
2022-01-14 18:35:18 -0300

    alter versions and requiriments

--- lib/active_pg_log.rb
+++ lib/active_pg_log.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 module ActivePgLog
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 end
 
+require 'active_record'
 require 'active_pg_log/active_pg_log_table'
 require 'active_pg_log/prepare'
 require 'active_pg_log/active_trigger'
