8ba7203b7ac867d9f32fa425edddf9dbc4e1e032
Wesley Oliveira <waejloliveira@gmail.com>
2022-01-14 18:27:43 -0300

    refactory requires

--- lib/active_pg_log.rb
+++ lib/active_pg_log.rb
@@ -4,7 +4,7 @@ module ActivePgLog
   VERSION = '0.1.2'
 end
 
-# require 'active_pg_log/active_pg_log_table'
+require 'active_pg_log/active_pg_log_table'
 require 'active_pg_log/prepare'
 require 'active_pg_log/active_trigger'
 require 'active_pg_log/active_log'
